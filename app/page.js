ba0ac0ab8d3121ef70a90c73cc253ff424664891
ゆう <imai_yuichi@liginc.co.jp>
2023-10-19 11:23:48 +0900

    skilsセクションの追加

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Blog from "@/app/components/BlogArticle";
 import Mv from "@/app/partials/IndexMv";
 import IndexAbout from "@/app/partials/IndexAbout";
+import IndexSkils from "@/app/partials/IndexSkils";
 
 const Home = () => {
   return (
     <main>
       <Mv />
       <IndexAbout />
+      <IndexSkils />
       <Blog />
     </main>
   );
